374e774f259c0df3952ec791d416ae73202fcd91
Yannick <yannickstreit@hotmail.com>
2025-09-06 00:58:47 +0800

    Styling fixes

--- js/sketch.js
+++ js/sketch.js
@@ -1,28 +1,38 @@
+const MIN_FONT_SIZE = 50;
+const MAX_FONT_SIZE = 100;
+const particles = [];
 let font;
-let particles = [];
 
 function preload() {
   font = loadFont("js/AvenirNextLTPro-Demi.otf");
 }
 
 function setup() {
-  const canvas = createCanvas(windowWidth, 400);
+  const canvas = createCanvas(windowWidth, windowHeight/3);
   canvas.parent("p5-canvas");
   const renderedText = "Hi, I'm Yannick"
 
-  const points = font.textToPoints(renderedText, 0, 0, 100, {
+  let fontSize = windowWidth / 12;
+  if (fontSize > MAX_FONT_SIZE) { 
+    fontSize = MAX_FONT_SIZE;
+  }
+  if (fontSize < MIN_FONT_SIZE) {
+    fontSize = MIN_FONT_SIZE;
+  }
+
+  const points = font.textToPoints(renderedText, 0, 0, fontSize, {
     sampleFactor: 0.3,
   });
 
   textFont(font);
-  textSize(100);
+  textSize(fontSize);
   const w = textWidth(renderedText);
   const h = textAscent() - textDescent();
 
   for (let i = 0; i < points.length; i++) {
     const pt = points[i];
     const offsetX = width / 2;
-    const offsetY = height / 2 - h / 2;
+    const offsetY = height / 2;
     const particle = new Particle(pt.x + offsetX - w / 2, pt.y + offsetY + h / 2);
     particles.push(particle);
   }
